a9b44a9e8df7429844e72e1220f33bc7a025741e
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-02-15 16:11:41 +0000

    Docking feature tested

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,17 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it { is_expected.to respond_to(:dock).with(1).argument}
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it { is_expected.to respond_to :bike }
 end
