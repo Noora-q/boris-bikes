04bf5dcf94f8df1fd624e4b141b0b0f0226e21ef
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-02-15 12:06:45 +0000

    First passing test

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,7 @@
+require 'docking_station'
+
+describe DockingStation do
+  # it 'returns "Bike released" when there is a bike' do
+  #   expect(docking_station()).eq to 'Bike released'
+  # end
+end
